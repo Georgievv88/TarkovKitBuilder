b7146ec1f82518d31cc823149216880f24598bae
Georgiev <georgievmarko66@gmail.com>
2026-05-19 04:28:32 +0200

    added fuzzy item search

--- serverback/controllers/tarkovController.js
+++ serverback/controllers/tarkovController.js
@@ -1,33 +1,53 @@
 const axios = require("axios");
+const Fuse = require("fuse.js");
+
+let cachedItems = null;
 
 const searchItems = async (req, res) => {
   try {
     const search = req.query.search || "";
 
-    const query = `
-      query {
-        itemsByName(name: "${search}") {
-          id
-          name
-          shortName
-          iconLink
-          avg24hPrice
-          lastLowPrice
-          basePrice
-          weight
-          types
-        }
-      }
-    `;
+    if (!search.trim()) {
+      return res.status(400).json({ message: "Search is required" });
+    }
 
-    const response = await axios.post(process.env.TARKOV_API, {
-      query,
+    if (!cachedItems) {
+      const query = `
+        query {
+          items {
+            id
+            name
+            shortName
+            iconLink
+            avg24hPrice
+            lastLowPrice
+            basePrice
+            weight
+            types
+          }
+        }
+      `;
+
+      const response = await axios.post(process.env.TARKOV_API, {
+        query,
+      });
+
+      cachedItems = response.data.data.items;
+    }
+
+    const fuse = new Fuse(cachedItems, {
+      keys: ["name", "shortName", "types"],
+      threshold: 0.35,
+      ignoreLocation: true,
+      includeScore: true,
     });
 
-    res.json(response.data.data.itemsByName);
+    const results = fuse.search(search).map((result) => result.item);
+
+    res.json(results.slice(0, 30));
   } catch (error) {
     console.log(error.message);
-    res.status(500).json();
+    res.status(500).json({ message: "Failed to search items" });
   }
 };
 
